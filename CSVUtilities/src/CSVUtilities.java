97a5c9a09af878e681004b1c91575d010c2cb813
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-12-19 13:14:49 -0500

    4.2

---
+++ CSVUtilities/src/CSVUtilities.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;;
+public class CSVUtilities {
+	ArrayList<String> CSVData;
+	int numColumns;
+	public CSVUtilities(File csv)
+	{
+		
+	}
+	public List<String> getColumnHeaders()
+	{
+		return String;
+	}
+	public List<String> getData(int column)
+	{
+		
+	}
+	public List<Integer> getData(int column)
+	{
+		
+	}
+	public List<Double> getData(int column)
+	{
+		
+	}
+
+}
