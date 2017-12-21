56ee36cffd1e81cbd970d08b8355f6d69d103054
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-12-21 13:13:34 -0500

    edited

--- CSVUtilities/src/Runner.java
+++ CSVUtilities/src/Runner.java
@@ -15,37 +15,40 @@ public static void main(String[] args)
 	}
 }
 	private static List<Water> readBooksFromCSV(String fileName) {
-	List<Water> books = new ArrayList<>(); Path pathToFile = Paths.get(fileName);
+	List<Water> waters = new ArrayList<>(); 
+	Path pathToFile = Paths.get(fileName);
 	
 	try (BufferedReader br = Files.newBufferedReader(pathToFile,StandardCharsets.US_ASCII))
 	{
 		String line = br.readLine();
 		while (line != null) {
 	String[] attributes = line.split(",");
-	Water book = createBook(attributes);
-	books.add(book);
+	Water water = createBook(attributes);
+	waters.add(water);
 	line = br.readLine();
 	}
 	}catch (IOException ioe) {
 	            ioe.printStackTrace();
 	}
-	return books;
+	return waters;
 }
 	private static Water createBook(String[] metadata)
 	{
-	String name = metadata[0];
-	int price = Integer.parseInt(metadata[1]);
-	String author = metadata[2];
-	return new Water(name, price, author);
+	String header =(metadata[0]);
+	int Year = Integer.parseInt(metadata[0]);
+	int Population = Integer.parseInt(metadata[1]);
+	int consumption = Integer.parseInt(metadata[2]);
+	int Gallons = Integer.parseInt(metadata[3]);
+	return new Water(Gallons, Year, Population, consumption, header);
 	}
 } 
 class Water {
 	private	int consumption;
 	private int Population;
 	private int Year;
-	private int Gallon;
+	private String Gallon;
 	private String header;
-public Water(int Gallon, int Year, int Population, int consumption, String header)
+public Water(String Gallon, int Year, int Population, int consumption, String header)
 	{this.Year = Year; this.consumption = consumption; this.Gallon = Gallon; this.Population = Population; this.header = header; }
 public String getHeader() {
     return header;
@@ -56,7 +59,6 @@ public int getPopulation() {
 }
 public void getYear(int Year) 
 { this.Year = Year; };
-public void setAuthor(String author) { this.author = author; }
 @Override
 public String toString() { return "Water [Year=" + Year + ", Gallon=" +
 Gallon + ", Population=" + Population + "]"; }
