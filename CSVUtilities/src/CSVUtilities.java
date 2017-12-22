3794aa83894da901d067ca369ed132dd3d7d36e0
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-12-22 13:08:42 -0500

    printed out data set, nice

--- CSVUtilities/src/CSVUtilities.java
+++ CSVUtilities/src/CSVUtilities.java
@@ -1,33 +1,61 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;;
+import java.util.Arrays;
+import java.util.List;
 public class CSVUtilities {
-	private ArrayList<String> CSVData;
-	
-	public CSVUtilities(File csv)
-	{
+	private static ArrayList<String> CSVData = new ArrayList<String>();
+	private int numColumn;
+	public CSVUtilities(File csv) throws IOException {
+		FileReader csv2 = new FileReader(csv);
 		
+		try (BufferedReader br = new BufferedReader(csv2)) {
+			String line = br.readLine();
+			while (line != null) {
+				CSVData.add(line);
+				line = br.readLine();
+			}
+		}
+		catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+		
+		this.numColumn = getColumnHeaders().size();
 	}
 	public List<String> getColumnHeaders()
 	{
-		return String;
+		return Arrays.asList(CSVData.get(0).split(","));
 	}
-	public List<String> getData(int column)
+	public static List<String> getDataString(int column)
 	{
-		
+		List<String> data = new ArrayList<String>();
+		for (int i = 1; i < CSVData.size(); i++) 
+		{
+			String[] row = CSVData.get(i).split(",");
+			data.add(row[column]);
+		}
+		return data;
 	}
-	public List<Integer> getData(int column)
-	{
-		
+	public static List<Integer> getDataInt(int column) {
+		ArrayList<Integer> data = new ArrayList<Integer>();
+		for (int x = 1; x < CSVData.size(); x++)
+		{
+			String[] temp = CSVData.get(x).split(",");
+			data.add(Integer.parseInt(temp[column]));
+		}
+		return data;
 	}
-	public List<Double> getData(int column)
+	public static List<Double> getDataDouble(int column) 
 	{
-		
+		ArrayList<Double> data = new ArrayList<Double>();
+		for (int x = 1; x < CSVData.size(); x++) 
+		{
+			String[] temp = CSVData.get(x).split(",");
+			data.add(Double.parseDouble(temp[column]));
+		}
+		return data;
 	}
 
 }
--- CSVUtilities/src/Runner.java
+++ CSVUtilities/src/Runner.java
@@ -1,65 +1,50 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;;
+import java.util.List;
+
+
 public class Runner {
-public static void main(String[] args)
-{
-	List<Water> water = readBooksFromCSV("water.csv");
-	for (Water b : water) {
-	     System.out.println(b);
-	}
-}
-	private static List<Water> readBooksFromCSV(String fileName) {
-	List<Water> waters = new ArrayList<>(); 
-	Path pathToFile = Paths.get(fileName);
-	
-	try (BufferedReader br = Files.newBufferedReader(pathToFile,StandardCharsets.US_ASCII))
+
+	public static void main(String[] args) throws IOException 
 	{
-		String line = br.readLine();
-		while (line != null) {
-	String[] attributes = line.split(",");
-	Water water = createBook(attributes);
-	waters.add(water);
-	line = br.readLine();
-	}
-	}catch (IOException ioe) {
-	            ioe.printStackTrace();
-	}
-	return waters;
-}
-	private static Water createBook(String[] metadata)
-	{
-	String header =(metadata[0]);
-	int Year = Integer.parseInt(metadata[0]);
-	int Population = Integer.parseInt(metadata[1]);
-	int consumption = Integer.parseInt(metadata[2]);
-	int Gallons = Integer.parseInt(metadata[3]);
-	return new Water(Gallons, Year, Population, consumption, header);
-	}
-} 
-class Water {
-	private	int consumption;
-	private int Population;
-	private int Year;
-	private String Gallon;
-	private String header;
-public Water(String Gallon, int Year, int Population, int consumption, String header)
-	{this.Year = Year; this.consumption = consumption; this.Gallon = Gallon; this.Population = Population; this.header = header; }
-public String getHeader() {
-    return header;
-}
-public void setHeader(String header) { this.header = header; }
-public int getPopulation() {
-    return Population;
-}
-public void getYear(int Year) 
-{ this.Year = Year; };
-@Override
-public String toString() { return "Water [Year=" + Year + ", Gallon=" +
-Gallon + ", Population=" + Population + "]"; }
+		File sat = new File("water.csv");
+		
+		CSVUtilities csv = new CSVUtilities(sat);
+		
+		List<String> heading = csv.getColumnHeaders();
+		for (String x: heading) 
+		{
+			System.out.println(x);
+		}
+		List<String> School = CSVUtilities.getDataString(0);
+		for(String x : School)
+		{
+			System.out.print(x+ " ");
+		}
+		System.out.println();
+		List<String> Name = CSVUtilities.getDataString(1);
+		for(String x : Name)
+		{
+			System.out.print(x+ " ");
+		}
+		System.out.println();
+		List<Integer> Takers = CSVUtilities.getDataInt(2);
+		for(int x : Takers)
+		{
+			System.out.print(x+ " ");
+		}
+		System.out.println();
+		List<Integer> Eng = CSVUtilities.getDataInt(3);
+		for(int x : Eng)
+		{
+			System.out.print(x+ " ");
+		}
+		System.out.println();
+		}
 }
